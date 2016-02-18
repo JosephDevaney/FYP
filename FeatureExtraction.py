d4ea0c456e85952e41ca737dab0029572513499a
JosephDevaney <joe.devaney1@gmail.com>
2016-02-18 01:47:12 +0000

    Reads objects form binary file. Checks all other incoming files to see if they have been loaded already. BUG: wav.read() returning data as strange ndarray

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -5,11 +5,18 @@ import pickle as pkl
 
 
 class VideoFeatures:
-    def __init__(self, file_data):
-        self.rate = file_data[0]
-        self.data = file_data[1].T[0]
-        rate = file_data[0]
-        data = file_data[1].T[0]
+    def __init__(self, rate, data, filename):
+        self.filename = filename
+
+        # self.rate = file_data[0]
+        # self.data = file_data[1]
+        # rate = file_data[0]
+        # data = file_data[1]
+        #
+        # data = data[0]
+        data = data.T[0]
+        self.rate = rate
+        self.data = data
 
         self.bvratio = wnl.beat_variance_ratio(rate, data)
 
@@ -22,21 +29,46 @@ class VideoFeatures:
         self.spectroid = wnl.spec_centroid(rate, data)
 
     def write_to_file(self, pickler=None):
+        print("***Writing {0} to disk***".format(self.filename))
         if pickler is None:
             file = "features.ftr"
-            with open(file) as output:
+            with open(file, 'a+') as output:
                 pkl.dump(self, output, -1)
         else:
             pickler.dump(self)
+        print("Finished writing {0} to file".format(self.filename))
+
 
 def main():
     path = input("Enter the filepath here: \n")
+    videos = {}
+    try:
+        with open(path + "features.ftr", "rb") as inp:
+            while True:
+                try:
+                    vid = pkl.load(inp)
+                    videos[vid.filename] = vid
+                except:
+                    print("EOF")
+                    break
+    except:
+        print("No file")
 
     # rate, data = wav.read(file)
-    with open(path + "\\features.ftr", "wb+") as output:
+    with open(path + "features.ftr", "ab") as output:
         pickler = pkl.Pickler(output, -1)
-        videos = [VideoFeatures(wav.read(path + file)).write_to_file(pickler) for file in os.listdir(path)
-                  if file.endswith('.wav')]
+        # [videos.update({file: VideoFeatures(wav.read(path + file), file).write_to_file(pickler)})
+        #  for file in os.listdir(path) if file.endswith('.wav') and file not in videos]
+        for file in os.listdir(path):
+            if file.endswith('.wav') and file not in videos:
+                rate, data = wav.read(path + file)
+                vid = VideoFeatures(rate, data, file)
+                vid.write_to_file(pickler)
+                videos[file] = vid
+
+
+
+    print("Dunzo!")
 
 
 if __name__ == "__main__":
